ec7392b658b40946ff5c2e0892463309e7f9417b
Ryoga1009 <mznryg@gmail.com>
2021-12-01 21:23:14 +0900

    add: スキャン画面用のアイテム追加

---
+++ BLELogger/app/src/main/java/com/ryoga/blelogger/item/BeaconRssiItem.kt
@@ -0,0 +1,26 @@
+package com.ryoga.blelogger.item
+
+import android.view.View
+import com.ryoga.blelogger.R
+import com.ryoga.blelogger.data.model.BeaconInfo
+import com.ryoga.blelogger.databinding.BeaconRssiListItemBinding
+import com.xwray.groupie.viewbinding.BindableItem
+
+class BeaconRssiItem(private val beaconInfo: BeaconInfo) :
+    BindableItem<BeaconRssiListItemBinding>() {
+
+    var rssi: Int = 0
+
+    override fun bind(viewBinding: BeaconRssiListItemBinding, position: Int) {
+        viewBinding.textViewBeaconName.text = beaconInfo.beaconName
+        viewBinding.textViewRssi.text = rssi.toString()
+    }
+
+    override fun getLayout(): Int = R.layout.beacon_rssi_list_item
+
+
+    override fun initializeViewBinding(view: View): BeaconRssiListItemBinding =
+        BeaconRssiListItemBinding.bind(view)
+
+
+}
