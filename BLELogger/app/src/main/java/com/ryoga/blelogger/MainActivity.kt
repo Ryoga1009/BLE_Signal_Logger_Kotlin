d5d275f629e1a9bfa137a32672316637bb4a75ca
Ryoga1009 <mznryg@gmail.com>
2021-12-03 23:24:56 +0900

    update: 処理を整理

--- BLELogger/app/src/main/java/com/ryoga/blelogger/MainActivity.kt
+++ BLELogger/app/src/main/java/com/ryoga/blelogger/MainActivity.kt
@@ -4,11 +4,10 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
 import com.ryoga.blelogger.databinding.MainActivityBinding
 import com.ryoga.blelogger.ui.beacon_info_list.BeaconInfoListFragment
 import org.altbeacon.beacon.BeaconManager
@@ -17,15 +16,15 @@ import org.altbeacon.beacon.Region
 
 class MainActivity : AppCompatActivity() {
     lateinit var mBeaconManager: BeaconManager
-    val BEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
-    val region = Region("all-beacons-region", null, null, null)
+    private val BEACON_FORMAT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"
+    private val region = Region("all-beacons-region", null, null, null)
 
-    lateinit var mViewModel: MainViewModel
+    private val mViewModel: MainViewModel by viewModels()
     private var _mBinding: MainActivityBinding? = null
     private val mBinding get() = _mBinding!!
 
 
-    val requestPermissionLauncher = registerForActivityResult(
+    private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted: Boolean ->
         if (!isGranted) {
@@ -36,44 +35,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         _mBinding = MainActivityBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        // 位置情報権限がない場合リクエスト
-        val permission = ContextCompat.checkSelfPermission(
-            applicationContext,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
-        if (permission == PackageManager.PERMISSION_DENIED) {
-            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-        }
+        checkPermission()
 
         mBeaconManager = BeaconManager.getInstanceForApplication(application)
         mBeaconManager.beaconParsers.add(BeaconParser().setBeaconLayout(BEACON_FORMAT))
 
         mBeaconManager.getRegionViewModel(region).rangedBeacons.observe(this, { beaconList ->
-            // 一旦トーストに検知できたビーコンの数を表示　
-            // ビーコンの電池買ってきたら色々実装後に削除
-            Toast.makeText(this, "Beaconの数：" + beaconList.size, Toast.LENGTH_SHORT).show()
-            beaconList.forEach { beacon ->
-
-            }
+            mViewModel.onBeaconObserved(beaconList)
         })
 
         mViewModel.mIsBeaconRaging.observe(this) {
             if (it) {
-                try {
-                    mBeaconManager.foregroundBetweenScanPeriod = 3000
-                    mBeaconManager.startRangingBeacons(region)
-                } catch (e: Exception) {
-                    Log.d("MYE", "onCreate: " + e.message)
-                }
+                startRanging()
             } else {
                 mBeaconManager.stopRangingBeacons(region)
             }
         }
-
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -85,5 +65,25 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         mBeaconManager.stopRangingBeacons(region)
+    }
+
+    private fun checkPermission() {
+        // 位置情報権限がない場合リクエスト
+        val permission = ContextCompat.checkSelfPermission(
+            applicationContext,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun startRanging() {
+        try {
+            mBeaconManager.foregroundBetweenScanPeriod = 3000
+            mBeaconManager.startRangingBeacons(region)
+        } catch (e: Exception) {
+            Log.d("MYE", "onCreate: " + e.message)
+        }
     }
 }
